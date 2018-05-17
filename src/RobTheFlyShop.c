4b669a2ab40575d1b32d184a0edd0d1fe8c6caf3
jeromkiller <jerom.venneker@gmail.com>
2018-05-18 00:29:57 +0200

    added header and footer, added funtion to serve page with header and footer

--- src/RobTheFlyShop.c
+++ src/RobTheFlyShop.c
@@ -8,26 +8,64 @@
 int		serve_index(struct http_request *);
 int		serve_linked(struct http_request *);
 
+//serve full page
+int serve_page(struct http_request *, u_int8_t *, size_t len);
+
 //actual functions
 int
 serve_index(struct http_request *req)
 {
-//	size_t		len;
-//	struct kore_buf	*res;
-//	u_int8_t	*data;
+	//to serve a page from a buffer, like with more dynamic content do the following:
+	//create a buffer
+	size_t		len;
+	struct kore_buf	*buff;
+	u_int8_t	*data;
 
-//	res = kore_buf_alloc(0);
-//	kore_buf_append(res, asset_index_html, asset_len_index_html);
+	buff = kore_buf_alloc(0);
 
-	http_response(req, 200, asset_index_html, asset_len_index_html);
+	//add the content you want to the buffer
+	kore_buf_append(buff, asset_index_html, asset_len_index_html);
 
-//	kore_buf_free(res);
+	//release the buffer, and get the length
+	data = kore_buf_release(buff, &len);
+	
+	//call serve page to get a pretty header and footer
+	serve_page(req ,data, len);
+
+	//free the buffer
+	kore_free(data);
 	return (KORE_RESULT_OK);
 }
 	
 int
 serve_linked(struct http_request *req)
-{
-	http_response(req, 200, asset_linked_html, asset_len_linked_html);
+{	
+	//to serve static content simply call serve page with the static content
+	serve_page(req, asset_linked_html, asset_len_linked_html);
 	return (KORE_RESULT_OK);
+}
+
+//serve page
+//this function takes a buffer containting the content of the page and sends an http_response for the full page
+int serve_page(struct http_request *req, u_int8_t *content, size_t content_length){
+	//create buffer for the full page
+	size_t		len;
+	struct kore_buf	*buff;
+	u_int8_t	*data;
+
+	buff = kore_buf_alloc(0);
+
+	//add the header, content and footer to the page
+	kore_buf_append(buff, asset_DefaultHeader_html, asset_len_DefaultHeader_html);
+	kore_buf_append(buff, content, content_length);
+	kore_buf_append(buff, asset_DefaultFooter_html, asset_len_DefaultFooter_html);
+	
+	//serve the page to the user
+	data = kore_buf_release(buff, &len);
+	http_response(req, 200, data, len);
+
+	//free the buffer for the full page
+	kore_free(data);
+
+	return(KORE_RESULT_OK);
 }

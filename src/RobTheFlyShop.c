6e6d761f6fed50fd04e1562c463f36fa4190a0fd
Jaap Groot <jaap.groot@hotmail.com>
2018-06-12 13:20:18 +0200

    User info page created

--- src/RobTheFlyShop.c
+++ src/RobTheFlyShop.c
@@ -281,10 +281,11 @@ int serve_admin_cancel_flight(struct http_request *req) {
 			kore_buf_replace_string(buf, "$searchFlightLoc$", fLoc, strlen(fLoc));
 		
 			//If the DB connection failed, show a error.
+	
 			if(!kore_pgsql_setup(&sql, "DB", KORE_PGSQL_SYNC)){
 				kore_pgsql_logerror(&sql);
 			}
-			else{	
+			else{
 				//Save the query in a var. Limit 10, because we don't want more then 10 results and only the last 10 results.
 				snprintf(query, sizeof(query), "SELECT * FROM flight WHERE location LIKE \'%%%s%%\' AND cancelled = 'f'  ORDER BY flight_date DESC LIMIT 10",fLoc);
 				//Return on the cmd which query is executed.
@@ -598,7 +599,71 @@ int serve_adminorders(struct http_request *req) {
 	return (KORE_RESULT_OK);
 }
 
-int serve_adminaccount(struct http_request *req) {
+//@description Back-end to serve the account info page
+//Input the http request.
+//output none
+int serve_account_info(struct http_request *req) {
+	char			*uID, *fName, *lName, *mail, *rMiles, query[150];
+	struct kore_buf		*buf;
+	u_int8_t		*data;
+	size_t 			len;
+	struct kore_pgsql	sql;
+	int			rows;
+	
+	//Clear all the vars of data.
+	fName = NULL;
+	lName = NULL;
+	mail = NULL;
+	rMiles = NULL;
+	uID = NULL;
+
+	//TODO Get the uID from the cookie now this is hardcoded for testing.
+	uID = "3";
+
+	buf = kore_buf_alloc(0);
+	kore_pgsql_init(&sql);
+	
+	//TODO show empty page with error if no uID is given in the cookie
+	//Add the html to the buffer.
+	if(uID == NULL) {
+		kore_buf_append(buf, asset_infoPageFail_html, asset_len_infoPageFail_html);
+		kore_log(LOG_ALERT, "Burp Rob, they are in!");
+	}	
+	else {
+		kore_buf_append(buf, asset_infoPage_html, asset_len_infoPage_html);
+
+		if(!kore_pgsql_setup(&sql, "DB", KORE_PGSQL_SYNC)){
+			kore_pgsql_logerror(&sql);
+		}			
+		else{
+			snprintf(query, sizeof(query), "SELECT * FROM users WHERE user_id = \'%s\'",uID);
+			//Return on the cmd which query is executed.
+			kore_log(LOG_NOTICE, "%s", query);
+			//If the query failed, show a error.
+			if(!kore_pgsql_query(&sql, query)){
+				kore_pgsql_logerror(&sql);
+			}
+			//Else query succesfully executed. 
+			else {	
+				fName = kore_pgsql_getvalue(&sql, 0, SQL_USERS_FIRST_NAME);
+				lName = kore_pgsql_getvalue(&sql, 0, SQL_USERS_LAST_NAME);
+				mail = kore_pgsql_getvalue(&sql, 0, SQL_USERS_MAIL);
+				rMiles = kore_pgsql_getvalue(&sql, 0, SQL_USERS_ROB_MILES);
+				
+				//Replace listenty with the new list, so it grows.
+				kore_buf_replace_string(buf, "$fName$", fName, strlen(fName));
+				kore_buf_replace_string(buf, "$lName$", lName, strlen(lName));
+				kore_buf_replace_string(buf, "$mail$", mail, strlen(mail));
+				kore_buf_replace_string(buf, "$rMiles$", rMiles, strlen(rMiles));
+			}
+		kore_pgsql_cleanup(&sql);
+		}
+	}
+
+
+	data = kore_buf_release(buf, &len);
+	serve_page(req, data, len);
+	kore_free(data);
 	return (KORE_RESULT_OK);
 }
 

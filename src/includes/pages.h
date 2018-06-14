d83a95fae903ba6c859692e597cec4f06c8aae79
jeromkiller <jerom.venneker@gmail.com>
2018-06-14 11:29:18 +0200

    added logout functionality

--- src/includes/pages.h
+++ src/includes/pages.h
@@ -3,6 +3,7 @@
 int		serve_index(struct http_request *);
 int		serve_login(struct http_request *);
 int		serve_logedin(struct http_request *);
+int		serve_logout(struct http_request *);
 int		serve_register(struct http_request *);
 int		serve_eula(struct http_request *);
 int		serve_adminflight(struct http_request *);

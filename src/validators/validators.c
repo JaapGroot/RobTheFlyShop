6a1785fc80cfe4891d6a1824ec26b5bd51928f10
jojilaan <uyilaan@gmail.com>
2018-06-14 12:56:27 +0200

    added validators

--- src/validators/validators.c
+++ src/validators/validators.c
@@ -1,7 +1,54 @@
 #include "includes.h"
 
-//Validator functions
+//Description:	Validator
+//@input:	
+//@output:	
 int v_admin_validate(struct http_request *req, char *data) {
-	//TODO: Moet nog gemaakt worden, momenteel voor testen admin page
-	return (KORE_RESULT_OK);
+	unsigned int 	uid, role;
+
+	uid = getUIDFromCookie(req);
+	role = getRoleFromUID(req);
+	
+	if(role == 1)
+	{
+		return (KORE_RESULT_OK);
+	}
+	else
+	{
+		return (KORE_RESULT_ERROR);
+	}
+}
+
+//Description:	Validator
+//@input:	
+//@output:	
+int v_user_validate(struct http_request *req, char *data) {
+	unsigned int 	uid, role;
+
+	uid = getUIDFromCookie(req);
+	role = getRoleFromUID(req);
+	
+	if(role == 0)
+	{
+		return (KORE_RESULT_OK);
+	}
+	else
+	{
+		return (KORE_RESULT_ERROR);
+	}
+}
+
+
+//Description:	Validator
+//@input:	
+//@output:	
+int v_generic_validate(struct http_request *req, char *data) {
+	if(v_user_validate(req, data) == 1 || v_admin_validate(req, data) == 1)
+	{
+		return (KORE_RESULT_OK);
+	}
+	else
+	{
+		return (KORE_RESULT_ERROR);
+	}
 }
